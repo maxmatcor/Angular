34034eb6acb3c623bcba0927c1d847e01e963ca7
maxmc <maxi_mc@live.com.ar>
2020-04-16 22:26:44 -0300

    1.2

---
+++ MiAngular/ClientApp/src/app/chat/chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+  selector: 'chat-app',
+  templetaUrl:'./chat.component.html'
+})
+
+export class ChatComponent {
+  public nombre ='sdfsdf'
+}
