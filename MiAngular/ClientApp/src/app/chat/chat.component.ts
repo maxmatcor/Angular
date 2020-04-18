2083382d28a80731dafd73368e786fe8b1656b21
maxmc <maxi_mc@live.com.ar>
2020-04-17 21:29:08 -0300

    levanta con mensaje

--- MiAngular/ClientApp/src/app/chat/chat.component.ts
+++ MiAngular/ClientApp/src/app/chat/chat.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
 
   selector: 'chat-app',
-  templetaUrl:'./chat.component.html'
+  templateUrl:'./chat.component.html'
 })
 
 export class ChatComponent {
