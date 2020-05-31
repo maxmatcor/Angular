9f3344366f7fb84879e443e4c18683f2c7f615c5
maxmc <maxi_mc@live.com.ar>
2020-05-31 19:12:18 -0300

    Start Angular One

--- MiAngular/ClientApp/src/app/message/message.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-message',
-  templateUrl:'./message.component.html'
-})
-export class MessageComponent {
-  @Input() texto: string;
-}
